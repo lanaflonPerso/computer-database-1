32cba54e7c09288c34aa76151a7c932424bd88c2
Vincent Galloy <vgalloy@excilys.com>
2016-02-25 16:33:24 +0100

    [ FIX      ] : Fix bad way to proceed when invalid user try to log

--- service/src/main/java/com/excilys/computerdatabase/service/services/impl/SecurityServiceImpl.java
+++ service/src/main/java/com/excilys/computerdatabase/service/services/impl/SecurityServiceImpl.java
@@ -11,13 +11,12 @@ import com.excilys.computerdatabase.service.validation.ServiceValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -105,7 +104,7 @@ public class SecurityServiceImpl implements SecurityService, UserDetailsService 
     public UserDetails loadUserByUsername(String username) {
         UserDetail userDetail = userDetailDao.getByUsername(username);
         if (userDetail == null) {
-            userDetail = new UserDetail("Not a user", "fake password", new HashSet<>());
+            throw new UsernameNotFoundException("No user found with username :" + username);
         }
         return userDetailsMapper.mapFromModel(userDetail);
     }
